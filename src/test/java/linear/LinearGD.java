5f982ec932290504291d410056bc6674dce4b305
sonyfe25cp <sonyfe25cp@gmail.com>
2013-02-25 00:43:18 +0800

    add test

---
+++ src/test/java/linear/LinearGD.java
@@ -0,0 +1,19 @@
+package linear;
+
+/**
+ * @author ChenJie
+ * 最后一个是结果，前面是特征
+ * [1,2,3,4,5]
+ * [2,1,3,4,5]
+ * [89,123,12,4,12]
+ * 
+ * y=f(x) = w*x
+ *
+ */
+public class LinearGD {
+
+	public static void main(String[] args) {
+
+	}
+
+}
